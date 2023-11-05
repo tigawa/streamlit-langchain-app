83e1269ef7c6db0a2905e63910e3d0a4b1dad3f0
ta.igawa <ihciiat@gmail.com>
2023-11-05 15:34:44 +0900

    チャットの会話履歴をふまえて応答する

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
+import os
 import streamlit as st
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
+
+load_dotenv()
+
+def create_agent_chain():
+    chat = ChatOpenAI(
+        model_name=os.environ["OPENAI_API_MODEL"],
+        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+    )
+
+    #OpenAI Functions AgentのプロンプトにMemoryの会話履歴を追加するために設定
+    agent_kwargs = {
+        'extra_prompt_messages': [MessagesPlaceholder(variable_name="memory")],
+    }
+    # OpenAI Functions Agentが使える設定でMemoryを初期化
+    memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+
+    tools = load_tools(["ddg-search", "wikipedia"])
+    return initialize_agent(tools,
+        chat,
+        agent=AgentType.OPENAI_FUNCTIONS,
+        agent_kwargs=agent_kwargs,
+        memory=memory,
+        )
 
 st.title("langchain-streamlit-app")
+
+if "agent_chain" not in st.session_state:
+    st.session_state.agent_chain = create_agent_chain()
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -16,8 +50,11 @@ if prompt:
 
     with st.chat_message("user"):
         st.markdown(prompt)
+
     with st.chat_message("assistant"):
-        response = "こんにちは"
+        callback = StreamlitCallbackHandler(st.container())
+        agent_chain = create_agent_chain()
+        response = st.session_state.agent_chain.run(prompt, callbacks=[callback])
         st.markdown(response)
 
     st.session_state.messages.append({"role":"assistant", "content": response})
